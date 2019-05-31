1b0784599c11bfa8fd8596950b8c5cf5f8450139
tinysammy <tinysammy@gmail.com>
2019-05-31 20:36:38 +0200

    created main package with hello world

--- go/json/test-output.go
+++ go/json/test-output.go
@@ -1,4 +1,7 @@
 // This initial file was created via Github Web Editor to go get it later on - Does it work?
 // go get github.com/tinysammy/play/go/json
+package main
 
+import "fmt"
 
+fmt.Println("Hello World")
